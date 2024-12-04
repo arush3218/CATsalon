787fa1e5ac4eb3027cf1c57950b94d60d8c444f4
arush3218 <arush3218@gmail.com>
2024-12-04 22:14:07 +0530

    first commit

---
+++ catsalon/base/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import Room
+from .forms import RoomForm, Room
+from django.shortcuts import redirect
+
+
+# rooms = [
+#     {'id': '1', 'name': 'Lets learn something'},
+#     {'id': '2', 'name': 'Dont learn something'},
+#     {'id': '3', 'name': 'Might learn something'},
+# ]
+
+def home(request):
+    rooms = Room.objects.all()
+    return render(request, 'base/home.html', {'rooms': rooms})
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+        
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/form.html',context)
+
