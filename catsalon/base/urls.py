3ba80742f22061de6a5620632c60ee798a7e9d8b
arush3218 <arush3218@gmail.com>
2024-12-07 00:05:12 +0530

    update #3

--- catsalon/base/urls.py
+++ catsalon/base/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('create-room/', views.createRoom, name = 'create_room'),
     path('update-room/<str:pk>', views.updateRoom, name = 'update_room'),
     path('delete-room/<str:pk>', views.DeleteRoom, name = 'delete_room'),
+    path('delete-message/<str:pk>', views.DeleteMessage, name = 'delete_message'),
     
 ]
--- catsalon/base/views.py
+++ catsalon/base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm, Room
 from django.db.models import Q
 from django.contrib.auth.models import User 
@@ -78,12 +78,27 @@ def home(request):
                                 )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages_raw = Message.objects.filter(Q(room__topic__name__icontains=q))
+    room_messages = room_messages_raw.order_by('-created')
+    
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html',context )
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+        
+    if request.method == 'POST':
+        messaage = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages': room_messages, 'particiapnts': participants}
         
     return render(request, 'base/room.html', context)
 
@@ -118,10 +133,21 @@ def updateRoom(request, pk):
 
 def DeleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+    if request.user != room.host :
+        return HttpResponse('You are not allowed here!')
     if request.method == 'POST':
         room.delete()
         redirect('home')
     return render(request, 'base/delete.html',{'obj':room})
+
+def DeleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user :
+        return HttpResponse('You are not allowed here!')
+    if request.method == 'POST':
+        message.delete()
+        redirect('home')
+    return render(request, 'base/delete.html',{'obj':message})
 
 
 
